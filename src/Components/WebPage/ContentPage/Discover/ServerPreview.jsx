e708ed446508ffa6f3c4ce3ebc4993cea5362029
Punnaphat Phiamtanapuwanon <punnaphat.zaza@gmail.com>
2024-04-18 05:29:41 +0700

    Delete section & Add WhiteSection

--- src/Components/WebPage/ContentPage/Discover/ServerPreview.jsx
+++ src/Components/WebPage/ContentPage/Discover/ServerPreview.jsx
@@ -1,4 +1,5 @@
 import { useLoaderData } from "react-router-dom";
+import WhiteSection from "./WhiteSection";
 import { MdVerified } from "react-icons/md";
 import ChatIcon from "../../../../assets/DiscoverPage/CommuServers/Midjourney/ChatIcon.svg";
 import CalendarIcon from "../../../../assets/DiscoverPage/CommuServers/Midjourney/CalendarIcon.svg";
@@ -10,7 +11,6 @@ import FacebookIcon from "../../../../assets/DiscoverPage/CommuServers/Midjourne
 import TwitterIcon from "../../../../assets/DiscoverPage/CommuServers/Midjourney/TwitterIcon.svg";
 import RedditIcon from "../../../../assets/DiscoverPage/CommuServers/Midjourney/RedditIcon.svg";
 import InstagramIcon from "../../../../assets/DiscoverPage/CommuServers/Midjourney/InstagramIcon.svg";
-import ServerPreImage from "../../../../assets/DiscoverPage/CommuServers/Midjourney/ServerPreImage.svg";
 
 const ServerPreview = () => {
     const loaderServerData = useLoaderData();
@@ -235,29 +235,7 @@ const ServerPreview = () => {
                     </div>
                 </div>
             </section>
-            <section className="w-full py-[120px] flex justify-center items-center bg-primary-w5">
-                <div className="max-w-[1260px] w-full px-10 grid grid-cols-12 gap-x-5">
-                    <div className="mb-4 mx-12 flex flex-col justify-center items-center col-span-5">
-                        <h2 className="mb-6 text-[32px] font-ggsans leading-10">
-                            Find a place where you belong
-                        </h2>
-                        <p className="mb-6 text-xl font-ggsansNormal leading-6">
-                            Discord servers are organized into topic-based
-                            channels where you can collaborate, share, and just
-                            talk about your day without clogging up a group
-                            chat.
-                        </p>
-                        <button className="w-[327px] h-[53px] py-4 px-8 text-xl text-white font-ggsansNormal bg-primary-b4 rounded-full leading-none">
-                            Join Discord
-                        </button>
-                    </div>
-                    <img
-                        className="col-span-7 col-start-6"
-                        src={ServerPreImage}
-                        alt=""
-                    />
-                </div>
-            </section>
+            <WhiteSection />
         </div>
     );
 };
