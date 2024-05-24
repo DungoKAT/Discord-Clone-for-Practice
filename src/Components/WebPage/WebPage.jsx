c26b110e221d26a4ee0b072f2bf1cb510fbe49d0
Punnaphat Phiamtanapuwanon <punnaphat.zaza@gmail.com>
2024-05-25 04:53:35 +0700

    Update Webpage
    Add Error page

--- src/Components/WebPage/WebPage.jsx
+++ src/Components/WebPage/WebPage.jsx
@@ -1,16 +1,18 @@
+import { useRouteError } from "react-router-dom";
 import MainNavbar from "./Navbar/MainNavbar";
 import MainFooter from "./Footer/MainFooter";
-// import HomePage from "./ContentPage/Home/HomePage";
 import { Outlet } from "react-router-dom";
+import WrongTurnPage from "./WrongTurnPage";
 
 const WebPage = () => {
+    const error = useRouteError();
     return (
         <div
             className="w-full h-full relative flex flex-col overflow-x-hidden overflow-y-scroll"
             id="web-page"
         >
             <MainNavbar />
-            <Outlet />
+            {error ? <WrongTurnPage /> : <Outlet />}
             <MainFooter />
         </div>
     );
